17b3e8048b1ab607919b2c2358b1689df6bcb29c
karenmcewen <karenf_email@yahoo.com>
2017-09-20 20:14:59 -0400

    classes added

--- BankAcctProject/Program.cs
+++ BankAcctProject/Program.cs
@@ -10,6 +10,18 @@ namespace BankAcctProject
     {
         static void Main(string[] args)
         {
+            // Program Class
+            // Must instantiate one client object
+            // Must instantiate one checking account object
+            // Must instantiate one savings account object
+            
+            Client client1 = new Client("Bob", "Smith", 555000);
+            Checking client1Checking = new Checking(555001, 250.75m);
+            Savings client1Savings = new Savings(555002, 1234.56m, 500.00m);
+
+            // All menu options listed above must have functionality behind them
+            // Program should run until user selects 'Exit'
+
         }
     }
 }
